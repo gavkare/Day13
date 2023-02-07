f37e38af0b51d26836cc9ac177aaa86cf6b733df
Shankar Gavkare <102272115+gavkare@users.noreply.github.com>
2023-02-07 07:58:02 +0530

    Add files via upload

--- MaximumNo.java
+++ MaximumNo.java
@@ -1,6 +1,6 @@
 package Day13;
 import java.util.Arrays;
-public class MaximumNO<T extends Comparable<T>> {
+public class MaximumNo<T extends Comparable<T>> {
     public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
         T max = x;
         if (y.compareTo(max) > 0)
@@ -29,5 +29,18 @@ public class MaximumNO<T extends Comparable<T>> {
 
     public void	FindMaximumNumbers(T x, T y, T z){
         testMax(x, y, z);
+    }
+
+
+
+
+    public static void main(String args[]) {
+        System.out.printf("Maximum of %d, %d and %d is %d\n\n", 3, 4, 5, maximum(3, 4, 5));
+        System.out.printf("Maximum of %.1f, %.1f and %.1f is %.1f\n\n", 6.6, 8.8, 7.7, maximum(6.6,
+                8.8, 7.7));
+        System.out.printf("Maximum of %s, %s and %s is %s\n", "Apple", "Peach", "Banana", maximum(
+                "Apple", "Peach", "Banana"));
+        FindMaximumNumbers findMaximumNumbers = new FindMaximumNumbers(10,20,30);
+        findMaximumNumbers.sort(new int [] {15,2,3});
     }
 }
