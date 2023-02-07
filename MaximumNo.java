6e8c62046603b0c9fadf3edcac53c18264ce3d8b
Shankar Gavkare <102272115+gavkare@users.noreply.github.com>
2023-02-07 07:53:35 +0530

    Add files via upload

--- MaximumNo.java
+++ MaximumNo.java
@@ -26,4 +26,8 @@ public class MaximumNO<T extends Comparable<T>> {
         Arrays.sort(sortElements);
         System.out.println(Arrays.toString(sortElements));
     }
+
+    public void	FindMaximumNumbers(T x, T y, T z){
+        testMax(x, y, z);
+    }
 }
