538e4265fd10e164538685fc7d4163b341621367
Shankar Gavkare <102272115+gavkare@users.noreply.github.com>
2023-02-07 07:49:31 +0530

    Add files via upload

--- MaximumNo.java
+++ MaximumNo.java
@@ -1,18 +1,24 @@
 package Day13;
 import java.util.Arrays;
 public class MaximumNo<T extends Comparable<T>> {
+    public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
+        T max = x;
+        if (y.compareTo(max) > 0)
+            max = y;
 
+        if (z.compareTo(max) > 0)
+            max = z;
 
-        public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
-            T max = x;
-            if (y.compareTo(max) > 0)
-                max = y;
+        return max;
+    }
+    public void testMaximum(T x, T y, T z) {
+        T max = x;
+        if (y.compareTo(max) > 0)
+            max = y;
 
-            if (z.compareTo(max) > 0)
-                max = z;
-
-            return max;
-        }
-
+        if (z.compareTo(max) > 0)
+            max = z;
+        System.out.println("Maximum value is :" +max);
     }
 
+}
